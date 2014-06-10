66b57c892d4d5344d3a6d3922450e7f851704cc5
dacm26 <dacm26@gmail.com>
2014-06-10 20:09:39 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmericanAirlines::Application.config.secret_key_base = '1c74271fd99af63839ca3e7d99bce41868a11f618b4b5ab1e041bd1824f5d9b201c7dd3bde26a6747c7fcda5775bae5414232cbd980be4b9b86ab099c0a920ac'
