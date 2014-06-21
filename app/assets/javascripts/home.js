160bd3260a140044e82c7ecb0f90ec75efbe05a2
Wilfredo Mejia <itha92@gmail.com>
2014-06-21 07:04:18 +0000

    home page bonito

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,11 @@
 $(function() {
+	$('#datetime_regreso').attr('disabled',true);
 	$('#datetime_ida').datepicker();
 	$('#datetime_regreso').datepicker();
+	$('#multi_1_1').datepicker();
+	$('#multi_2_1').datepicker();
+	$('#multi_3_1').datepicker();
+	$('#multi_4_1').datepicker();
   });
 
 
@@ -8,12 +13,32 @@ $('.type').change(function(){
 	switch ($(this).val()){
 		case "1":
 			$('#datetime_regreso').attr('disabled',true);
+			$('.more').hide();
+			$('.case_1').show();
+			$('.case_3').hide();
 			break;
 		case "2":
 			$('#datetime_regreso').attr('disabled',false);
+			$('.more').hide();
+			$('.case_1').show();
+			$('.case_3').hide();
 			break;
 		case "3":
 			$('#datetime_regreso').attr('disabled',false);
+			$('.more').show();
+			$('.case_1').hide();
+			$('.case_3').show();
 			break;
 	}
+});
+
+$('.more').on('click',function(){
+	$('.case_3').children().each(function()
+	{
+		if( $(this).css('display') === 'none')
+		{
+			$(this).show();
+		}
+	});
+	$(this).hide();
 });
